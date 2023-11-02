19c72f0fe966abbff6a3afee081576b37321aa3f
apzhou0 <apzhou@ucsb.edu>
2023-11-02 15:04:40 -0700

    azhou - Added articlesFixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,42 @@
+const articlesFixtures = {
+    oneArticle:
+    [
+      {
+        "id": 1,
+        "title": "Bird Article",
+        "url": "randomurl.com",
+        "explanation": "randomemail@gmail.com",
+        "LocalDateTime": "2000-01-10T:00:10:02"
+      }
+    ],
+
+    threeArticles:
+    [
+        {
+            "id": 2,
+            "title": "Cat Article",
+            "url": "caturl.com",
+            "explanation": "catemail@gmail.com",
+            "LocalDateTime": "2002-01-10T:00:10:02"    
+        },
+
+        {
+            "id": 3,
+            "title": "Dog Article",
+            "url": "dogurl.com",
+            "explanation": "dogemail@gmail.com",
+            "LocalDateTime": "2003-01-10T:00:11:02"
+        },
+
+        {
+            "id": 4,
+            "title": "Lizard Article",
+            "url": "lizardurl.com",
+            "explanation": "lizardemail@gmail.com",
+            "LocalDateTime": "2020-03-10T:00:10:02"    
+        },
+        
+    ]
+};
+
+export { articlesFixtures };
