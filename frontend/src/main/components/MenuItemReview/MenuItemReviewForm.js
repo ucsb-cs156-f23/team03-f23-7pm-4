012247a63bc5e1684d2b14f19740444e56a7a9e0
Alice Zhong <alicezhong@ucsb.edu>
2023-11-07 14:58:21 -0800

    az-WIP

---
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -0,0 +1,145 @@
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker restore all
+
+    const navigate = useNavigate();
+
+    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+    // Note that even this complex regex may still need some tweaks
+
+    // Stryker disable next-line Regex
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    // Stryker disable next-line all
+    //const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
+    const testIdPrefix = "MenuItemReview";
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+                {initialContents && (
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="itemId">itemId</Form.Label>
+                            <Form.Control
+                                data-testid={testIdPrefix + "-id"}
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                )}
+
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="itemId">itemId</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-itemId"}
+                        id="itemId"
+                        type="text"
+                        isInvalid={Boolean(errors.itemId)}
+                        {...register("itemId", {
+                            required: "ItemId is required.",
+                        })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.name?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                <Form.Group className="mb-3" >
+                <Form.Label htmlFor="reviewerEmail">reviewerEmail</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-reviewerEmail"}
+                    id="reviewerEmail"
+                    type="text"
+                    isInvalid={Boolean(errors.itemId)}
+                    {...register("reviewerEmail", {
+                        required: "Reviewer email is required.",
+                    })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.name?.message}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3" >
+                <Form.Label htmlFor="stars">stars</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-stars"}
+                    id="stars"
+                    type="text"
+                    isInvalid={Boolean(errors.itemId)}
+                    {...register("stars", {
+                        required: "Stars are required.",
+                    })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.name?.message}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateReviewed">dateReviewed</Form.Label>
+                        <Form.Control
+                            data-testid={testIdPrefix +"-dateReviewed"}
+                            id="dateReviewed"
+                            type="text"
+                            isInvalid={Boolean(errors.dateReviewed)}
+                            {...register("dateReviewed", { required: true, pattern: isodate_regex })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.dateReviewed && 'Date reviewed is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                <Form.Group className="mb-3" >
+                <Form.Label htmlFor="comments">comments</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-comments"}
+                    id="comments"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("comments", {
+                        required: "Comments are required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+
+            <Button
+                type="submit"
+                data-testid={testIdPrefix + "-submit"}
+            >
+                {buttonLabel}
+            </Button>
+            <Button
+                variant="Secondary"
+                onClick={() => navigate(-1)}
+                data-testid={testIdPrefix + "-cancel"}
+            >
+                Cancel
+            </Button>
+
+        </Form>
+
+    )
+}
+
+export default MenuItemReviewForm;
---
+++ frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.js
@@ -0,0 +1,102 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import MenuItemReview from "main/components/MenuItemReview/MenuItemReviewForm";
+import { menuItemReviewFixtures } from "fixtures/menuItemReviewFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+describe("MenuItemReview tests", () => {
+    const queryClient = new QueryClient();
+
+    const expectedHeaders = ["ItemId", "ReviewerEmail", "Stars", "DateReviewed","Comments"];
+    const testId = "MenuItemReviewForm";
+
+    test("renders correctly with no initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+    });
+
+    test("renders correctly when passing in initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm initialContents={menuItemReviewFixtures.oneMenuItemReview} />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+        expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
+        expect(screen.getByText(`Id`)).toBeInTheDocument();
+    });
+
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm />
+                </Router>
+            </QueryClientProvider>
+        );
+        expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+        const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+        fireEvent.click(cancelButton);
+
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+    });
+
+    test("that the correct validations are performed", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <MenuItemReviewForm />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+        const submitButton = screen.getByText(/Create/);
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/ItemId is required/);
+        expect(screen.getByText(/Comments are required/)).toBeInTheDocument();
+
+        const nameInput = screen.getByTestId(`${testId}-name`);
+        fireEvent.change(nameInput, { target: { value: "a".repeat(31) } });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
+        });
+    });
+
+});
