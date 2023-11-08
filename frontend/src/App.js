8fe4795c1e1ee35002b91652505769585aa3ffee
audoreven <azhu@ucsb.edu>
2023-11-08 04:36:47 -0800

    adding files for ucsb org table except tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,6 +61,21 @@ function App() {
             </>
           )
         }
+        {
+         hasRole(currentUser, "ROLE_USER") && (
+           <>
+             <Route exact path="/ucsborganizations" element={<PlaceholderIndexPage />} />
+           </>
+         )
+       }
+       {
+         hasRole(currentUser, "ROLE_ADMIN") && (
+           <>
+             <Route exact path="/ucsborganizations/edit/:id" element={<PlaceholderEditPage />} />
+             <Route exact path="/ucsborganizations/create" element={<PlaceholderCreatePage />} />
+           </>
+         )
+       }
          {
           hasRole(currentUser, "ROLE_USER") && (
             <>
