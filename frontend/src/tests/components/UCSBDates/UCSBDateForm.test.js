c4ca540bf1929ccba8998f4e28e08f93ae7724c4
apzhou0 <91772918+apzhou0@users.noreply.github.com>
2023-11-08 17:22:26 -0800

    Undid changes to UCSBDateForm.test.js

--- frontend/src/tests/components/UCSBDates/UCSBDateForm.test.js
+++ frontend/src/tests/components/UCSBDates/UCSBDateForm.test.js
@@ -79,6 +79,7 @@ describe("UCSBDateForm tests", () => {
 
         const mockSubmitAction = jest.fn();
 
+        
         render(
             <Router  >
                 <UCSBDateForm submitAction={mockSubmitAction} />
