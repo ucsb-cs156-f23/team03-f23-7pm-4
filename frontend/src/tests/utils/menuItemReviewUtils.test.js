39e10dde5b5dd639b3e332d1bf6d2c317730ab63
Alice Zhong <alicezhong@ucsb.edu>
2023-11-10 21:58:31 -0800

    test

--- frontend/src/tests/utils/menuItemReviewUtils.test.js
+++ frontend/src/tests/utils/menuItemReviewUtils.test.js
@@ -43,7 +43,7 @@ describe("menuItemReviewUtils", () => {
 
             // assert
             expect(result).toEqual({
-                url: "/api/menuItemReviews",
+                url: "/api/menuitemreview",
                 method: "DELETE",
                 params: { id: 17 }
             });
