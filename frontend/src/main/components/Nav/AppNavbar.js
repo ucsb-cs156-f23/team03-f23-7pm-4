b6e06afa440abd1526983fdc0adb2ef97fadeceb
Benny Conte <benconte223@gmail.com>
2023-11-11 16:20:42 -0800

    I think its fixed started over

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -56,10 +56,12 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                 <>
                   <Nav.Link as={Link} to="/restaurants">Restaurants</Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdates">UCSB Dates</Nav.Link>
+                  <Nav.Link as={Link} to="/RecommendationRequest">RecommendationRequest</Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
                 </>
               )
             }
+
             <Nav className="ml-auto">
               {
                 currentUser && currentUser.loggedIn ? (
