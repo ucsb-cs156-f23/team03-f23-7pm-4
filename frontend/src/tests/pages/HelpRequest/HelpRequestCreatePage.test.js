51c41e7ede1131cfcfc78566a2cb9616f319c9a6
Aleksandar Nisavic <anisavic@ucsb.edu>
2023-11-11 16:15:44 -0800

    AN - Deleted unused variables

--- frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
+++ frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
@@ -7,9 +7,9 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
-import { HelpRequestForm } from "main/components/HelpRequest/HelpRequestForm";
-import HelpRequestEditPage from "main/pages/HelpRequest/HelpRequestEditPage";
+//import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
+//import { HelpRequestForm } from "main/components/HelpRequest/HelpRequestForm";
+//import HelpRequestEditPage from "main/pages/HelpRequest/HelpRequestEditPage";
 
 const mockToast = jest.fn();
 jest.mock('react-toastify', () => {
